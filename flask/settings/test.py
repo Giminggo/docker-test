36712aa79380f78aeb0937e6964c20ccb4d0e038
Antoliny <antoliny0919@naver.com>
2023-07-08 17:30:12 +0900

    postgres db update

--- flask/settings/test.py
+++ flask/settings/test.py
@@ -1,5 +1,5 @@
 from settings.base import *
 
-SQLALCHEMY_DATABASE_URI = f'postgresql://{get_secret("POSTGRES_USER")}:{get_secret("POSTGRES_PASSWORD")}@localhost:5432/antoliny'
+SQLALCHEMY_DATABASE_URI = f'postgresql://{get_secret("POSTGRES_USER")}:{get_secret("POSTGRES_PASSWORD")}@{get_secret("POSTGRES_IP")}:{get_secret("POSTGRES_PORT")}/{get_secret("POSTGRES_DATABASE_NAME")}'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\xfaNt\xf5k-\xc0\x0e\xcb\xf4srb\xd2\xca3'
