5cbf74f0c96cf1410331e9af9ab29053094fa0d7
Antoliny <antoliny0919@naver.com>
2023-07-10 20:52:44 +0900

    change request url

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -8,6 +8,11 @@ export default function Home() {
   return (
     <>
       <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
+      <h1>hello world</h1>
     </>
   )
 }
